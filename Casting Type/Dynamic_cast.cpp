ee536f60d1c40d2ad9eae22ec2ebe3c436fecb2f
Souad gamal <40209323+souad88@users.noreply.github.com>
2022-09-25 00:21:04 +0200

    casting Operator

---
+++ Casting Type/Dynamic_cast.cpp
@@ -0,0 +1,10 @@
+//Uses of Dynamic_Casting:-
+// When we dealing with an abstract interface class( Polymorphism) 
+//then we have to use aPOINTER to point to a base class ,but we want to access only the information
+//in  the derived class.
+//-------------------------------------------
+//Then we have:- 1-Base class Interface-class with methods.
+//               2-Derived class .
+//               3- apointer to  a Base class.
+//               4- Apointer to abase-class.
+//               5- Dynamic casting to use the ONLY-INfOrmations  in the derived class without 
