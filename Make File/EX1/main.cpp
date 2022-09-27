c3aea185d3cf2e470e742ae1e1d6e754eb44c9a9
Souad gamal <40209323+souad88@users.noreply.github.com>
2022-09-27 23:04:42 +0200

    Create main.cpp

---
+++ Make File/EX1/main.cpp
@@ -0,0 +1,13 @@
+///creat aprog that print message class
+
+#include<iostream>
+#include "prnt.h"
+using namespace std;
+class prnt;
+int main()
+{  string ph1,ph2;
+    prnt p;
+  p.display(ph1,ph2);
+
+    return 0;
+}
