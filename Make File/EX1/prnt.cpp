a6555db38f3400cecf25d03f37feb2793a245ddc
Souad gamal <40209323+souad88@users.noreply.github.com>
2022-09-27 23:05:48 +0200

    Create prnt.cpp

---
+++ Make File/EX1/prnt.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "prnt.h"
+using namespace std;
+void prnt:: display(string ph1,string ph2)
+{
+
+ cout<<"Really , i love hot and cold coffee , what about you ?!"<<endl;
+ cin>>ph1;       
+ cout<<"also i love the winter ..Mmmm i think its a wonderfull when the rain water fall on my flowers..Do YOU THINK SO?"<<endl;  
+ cin>>ph2;
+ cout<<"if you love the same things , i think that you will be my best friend "<<endl;
+
+ }
+    
+    
+        
